ea647e13896785d5d7245472af5d0763c861d8bc
DoHieu1 <114351140+DoHieu1@users.noreply.github.com>
2023-05-12 12:23:20 +0700

    Update index.js

--- btck/node_review/index.js
+++ btck/node_review/index.js
@@ -1,9 +1,33 @@
 const express = require("express");
-const { connectToDb, db } = require("./db");
-
 const app = express();
+const { client } = require("./config/connectDB.js");
+const { inventoryRouter } = require('./routers/inventoryRouter.js');
+const { orderRouter } = require('./routers/orderRouter.js');
+const { userRouter } = require("./routers/userRouter.js");
+const { config } = require("dotenv");
+config();
 
-app.listen(3000, () => {
-  console.log("App is running at 3000");
-  connectToDb();
-});
+const PORT = process.env.PORT;
+
+async function main() {
+  try {
+    await client.connect();
+    console.log("Connected to mongodb successfully");
+
+    app.use(express.json());
+
+    app.use("/api/v1/user", authRouter);
+    app.use("/api/v1/inventory", inventoryRouter)
+    app.use("/api/v1/order", orderRouter)
+
+    app.listen(PORT, () => {
+      console.log(`App is running at ${PORT}`);
+    });
+
+  } catch (error) {
+
+    console.log(error.message)
+  }
+}
+
+main();
