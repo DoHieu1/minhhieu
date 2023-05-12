c14928a26ec96f992a1e21cd044ee68b421ea9a0
DoHieu1 <114351140+DoHieu1@users.noreply.github.com>
2023-05-12 12:25:05 +0700

    Update inventoryRouter.js

---
+++ btck/node_review/routers/inventoryRouter.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { inventoryCollection } = require("../config/connectDB.js");
+const { authMiddleware } = require("../middlewares/authMiddleware.js");
+
+router.get("/", authMiddleware, async (req, res) => {
+
+    try {
+        const { lowQty } = req.query
+        const input = lowQty ? { instock: { $lt: +lowQty } } : {}
+
+        const allInventory = await inventoryCollection.find(input).toArray();
+        res.status(200).json({
+            message: "Success",
+            data: allInventory,
+        })
+    }
+
+    catch (error) {
+        res.status(400).json({
+            message: "Error!!!",
+            data: error.message,
+        })
+    }
+
+})
+
+
+
+module.exports = { inventoryRouter: router }
